2b09e4231bdac8de72fbb7ade91b07cfaa56b324
JTS <jtshor@gmail.com>
2022-07-05 20:38:24 -0700

    got rid of some unnecessary variables

--- innerprodpolysolver.py
+++ innerprodpolysolver.py
@@ -4,16 +4,13 @@
 # q = x + x^2
 
 x = [1, 2, 3, 4]
-p = 0;
-q = 0;
-holder = 0;
 n = 0;
 
 for i in range(len(x)):
     # print(str(x[i]) + " is the number")
-    holder = (x[i] + x[i]**3)*(1 + x[i]**2)
+    n += (x[i] + x[i]**3)*(1 + x[i]**2)
     # print(str(holder) + " is the result")
-    n += holder
+    # n += holder
 
 print(n)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
